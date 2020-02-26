59ce527bbf894f0df94639c4d5421620fdb54046
Chris Eyrich <chriseyrich@gmail.com>
2020-02-26 15:20:37 -0500

    Add project files.

---
+++ JournalLibrary/Models/BookModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JournalLibrary.Models
+{
+    class BookModel
+    {
+        int ID { get; set; }
+        string BookName { get; set; }
+        string AuthorName { get; set; }
+        List<CategoryModel> Categories { get; set; }
+        double Price { get; set; }
+        bool Read { get; set; }
+    }
+}
---
+++ JournalLibrary/Models/CategoryModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JournalLibrary.Models
+{
+    class CategoryModel
+    {
+        int ID { get; set; }
+        string CategoryName { get; set; }
+    }
+}
