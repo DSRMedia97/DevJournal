d973fed89cbd4f8774b02d47f0d1f0ff7f997468
Chris Eyrich <chriseyrich@gmail.com>
2020-03-03 20:00:34 -0500

    Started building out SQL Connector to connect application to database

---
+++ JournalLibrary/DataConnectors/SQLConnector.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JournalLibrary.Models;
+
+namespace JournalLibrary.DataConnectors
+{
+    class SQLConnector : IDataConnector
+    {
+        public void CreateBookModel(BookModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateCategoryModel(CategoryModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<BookModel> LoadAllBooks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CategoryModel> LoadAllCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateBookModel(BookModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
