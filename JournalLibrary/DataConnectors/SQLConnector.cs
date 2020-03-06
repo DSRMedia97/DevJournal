58da292ab21b921861e5e361e2eadd2365fb8a9a
Chris Eyrich <chriseyrich@gmail.com>
2020-03-05 20:08:32 -0500

    Finished updating SQLConnector to allow links between books and categories

--- JournalLibrary/DataConnectors/SQLConnector.cs
+++ JournalLibrary/DataConnectors/SQLConnector.cs
@@ -27,6 +27,16 @@ namespace JournalLibrary.DataConnectors
                 connection.Execute("spBooks_Insert", p, commandType: CommandType.StoredProcedure);
 
                 model.ID = p.Get<int>("@id");
+
+                foreach (CategoryModel cm in model.Categories)
+                {
+                    p = new DynamicParameters();
+
+                    p.Add("@Bookid", model.ID);
+                    p.Add("@Categoryid", cm.ID);
+
+                    connection.Execute("spBookCategories_Insert", p, commandType: CommandType.StoredProcedure);
+                }
             }
         }
 
@@ -52,6 +62,15 @@ namespace JournalLibrary.DataConnectors
             using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(GlobalConfig.CnnString(db)))
             {
                 output = connection.Query<BookModel>("dbo.spBooks_GetAll").ToList();
+
+                var p = new DynamicParameters();
+
+                foreach (BookModel b in output)
+                {
+                    p = new DynamicParameters();
+                    p.Add("@Bookid", b.ID);
+                    b.Categories = connection.Query<CategoryModel>("spBookCategories_GetByBook", p, commandType: CommandType.StoredProcedure).ToList();
+                }
             }
 
             return output;
@@ -71,7 +90,78 @@ namespace JournalLibrary.DataConnectors
 
         public void UpdateBookModel(BookModel model)
         {
-            
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(GlobalConfig.CnnString(db)))
+            {
+                var p = new DynamicParameters();
+
+                p.Add("@BookName", model.BookName);
+                p.Add("@AuthorName", model.AuthorName);
+                p.Add("@Price", model.Price);
+                p.Add("@HasRead", model.Read);
+                p.Add("@id", model.ID);
+
+                //call sp to update book model -- similar to CreateBook but without the output id
+                connection.Execute("dbo.spBooks_Update", p, commandType: CommandType.StoredProcedure);
+
+                //call sp to get list of stored Categories for the book
+                List<CategoryModel> storedCategories = new List<CategoryModel>();
+                p = new DynamicParameters();
+                p.Add("@Bookid", model.ID);
+                storedCategories = connection.Query<CategoryModel>("spBookCategories_GetByBook", p, commandType: CommandType.StoredProcedure).ToList();
+
+                //if model.Categories list and storedCategories are both null then do nothing - nothing to update
+                if (model.Categories != null && storedCategories != null)
+                {
+                    //do checks
+                    foreach (CategoryModel c in model.Categories)
+                    {
+                        //if it exists in the database remove it from further actions
+                        if (storedCategories.Exists(x => x.ID == c.ID))
+                        {
+                            storedCategories.RemoveAt(storedCategories.FindIndex(x => x.ID == c.ID));
+                        }
+                        else //it doesn't already exist in the database so add it
+                        {
+                            p = new DynamicParameters();
+                            p.Add("@Bookid", model.ID);
+                            p.Add("@Categoryid", c.ID);
+                            connection.Execute("spBookCategories_Insert", p, commandType: CommandType.StoredProcedure);
+                        }
+                    }
+                    if (storedCategories.Count > 0)
+                    {
+                        foreach (CategoryModel storedC in storedCategories)
+                        {
+                            p = new DynamicParameters();
+                            p.Add("@Bookid", model.ID);
+                            p.Add("@Categoryid", storedC.ID);
+                            connection.Execute("spBookCategories_Remove", p, commandType: CommandType.StoredProcedure);
+                        }
+                    }
+                }
+                else if (model.Categories == null && storedCategories != null)
+                {
+                    //remove all from db in stored categories since nothing is assigned to the model categories
+                    foreach (CategoryModel c in storedCategories)
+                    {
+                        p = new DynamicParameters();
+                        p.Add("@Bookid", model.ID);
+                        p.Add("@Categoryid", c.ID);
+                        connection.Execute("spBookCategories_Remove", p, commandType: CommandType.StoredProcedure);
+                    }
+                }
+                else if (model.Categories != null && storedCategories == null)
+                {
+                    //add all from model.categories to db since nothing was previously saved in db
+                    foreach (CategoryModel c in model.Categories)
+                    {
+                        p = new DynamicParameters();
+                        p.Add("@Bookid", model.ID);
+                        p.Add("@Categoryid", c.ID);
+                        connection.Execute("spBookCategories_Insert", p, commandType: CommandType.StoredProcedure);
+                    }
+                }
+            }
         }
     }
 }
